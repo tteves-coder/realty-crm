a37e5f4f8209517e8e1bb2d9baf9543789e55065
tteves-coder <tteves@gmail.com>
2026-05-01 23:57:16 -0700

    Update page.tsx

--- app/dashboard/priority/page.tsx
+++ app/dashboard/priority/page.tsx
@@ -1,5 +1,6 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { ui } from "@/components/uiStyles";
 import PriorityList from "@/components/priority/PriorityList";
 export default async function Page() {
   const supabase = createServerComponentClient({ cookies });
